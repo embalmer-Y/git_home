1ff90c0ac4ce586f3933bd0955915c169dba0a5d
embalmer-Y <y19858120112@163.com>
2020-05-21 11:01:29 +0800

    mychat0.1

---
+++ client.py
@@ -0,0 +1,93 @@
+import socket
+import base64
+import hashlib
+import sys
+import time
+from progressbar import *
+
+"""
+作者:emb
+版权:你觉得呢
+创作日期:2020.05.20
+"""
+
+
+#全局变量
+# worning_level0 = 0
+worning_level1 = 0
+# worning_level2 = 0
+
+
+def prog():
+    print('正在上流')
+    progress = ProgressBar()
+    for i in progress(range(520)):
+        time.sleep(0.01)
+    return None
+
+
+def base_msg_decode(msg_bas):
+    msg_utf = base64.b64decode(msg_bas)
+    msg = msg_utf.decode('UTF-8')
+    return msg
+
+
+def base_msg_encode(msg):
+    msg_utf = msg.encode('UTF-8')
+    msg_bas = base64.b64encode(msg_utf)
+    return msg_bas
+
+
+def login():
+    print('=============上流聊天软件V0.1=============')
+    print('''------------------------------------------
+    |               使用说明                 |
+    | 想使用该软件请先给自己取一个牛皮的名字 |
+    | 1 加入聊天:输入聊天室密钥即可加入聊天  |
+    | 2 创建聊天:该选项将生成一个聊天室密钥  |
+    | 0 退出程序:关闭牛皮的上流聊天软件      |
+    |    加入聊天后可输入menu查看可选菜单    |
+    ------------------------------------------''')
+    user_name = input('请输入您牛皮的id:')
+    print('id设置成功')
+    options = input('请输入对应序号并按回车选择选项:')
+    if options == '2':
+        md5 = hashlib.md5()
+        md5.update(user_name.encode('utf-8'))
+        passwd = md5.hexdigest()
+        print('您的密钥为:')
+        print(passwd)
+        return (user_name ,passwd)
+    elif options == '1':
+        passwd = input('请键入上流聊天室密钥')
+        return (user_name ,passwd)
+    else:
+        global worning_level1
+        worning_level1 = worning_level1 + 1
+        if worning_level1 == 1:
+            print('再乱输入你就要出大事了')
+        elif worning_level1 == 2:
+            print('老铁,再一再二不再三')
+        elif worning_level1 == 3:
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    key = login()
+    print(key)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 建立连接:
+    s.connect(('119.3.219.105', 3389))
+    # 接收欢迎消息:
+    print(s.recv(1024).decode('utf-8'))
+    s.send(base_msg_encode(str(key)))
+    prog()
+    while True:
+        msg=input(">>>")
+        # 发送数据:
+        s.send(base_msg_encode(msg))
+        print(base_msg_decode(s.recv(1024)))
+        if msg =='exit':
+            break
+    s.send(base_msg_encode('exit'))
+    s.close()
---
+++ server.py
@@ -0,0 +1,123 @@
+import socket
+import threading
+import time
+import base64
+import hashlib
+
+"""
+作者:emb
+版权:你觉得呢
+创作日期:2020.05.20
+"""
+
+# 全局变量
+list_key = []
+list_name = []
+dict_msg = {}
+dict_key = {}
+
+
+def key_home(name ,passwd):
+    global list_key ,list_name ,dict_key ,dict_msg
+    list_name.append(name)
+    if passwd in list_key:
+        dict_key[passwd].append(name)
+    else:
+        list_key.append(passwd)
+        dict_key.update({passwd:[name]})
+        dict_msg.update({passwd:[]})
+
+
+def tuple_list(tuple_key):
+    str_key = ''
+    for i in tuple_key:
+        if i == '(' or i == ')' or i == ' ' or i == "'":
+            pass
+        else:
+            str_key = str_key + i
+    list_key_in = str_key.split(',',1)
+    return list_key_in
+
+
+
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome! connect server successful!')
+    time.sleep(1)
+    key = sock.recv(1024)
+    name ,passwd = tuple_list(tuple(base_msg_decode(key)))
+    key_home(name ,passwd)
+    msg_out(name ,passwd)
+    # while True:
+    #     data = sock.recv(1024)
+    #     time.sleep(1)
+    #     if not data or data.decode('utf-8') == 'exit':
+    #         break
+    #     print('Client:%s'%data.decode('utf-8'))
+    #     sock.send(('Server:%s'% input()).encode('utf-8'))
+    sock.close()
+    global dict_key
+    dict_key[passwd].pop(name)
+    print('Connection from %s:%s closed.' % addr)
+
+
+def msg_out(name ,passwd):
+    menu = '''
+            menu:
+            exit : 退出聊天室
+            show : 显示当前聊天室在线人数
+            lrs  ：启动狼人杀(完善中)
+            xg   ：连接到雪糕(完善中)
+            '''
+    while True:
+        msg = base_msg_decode(sock.recv(1024))
+        time.sleep(2)
+        if not msg or msg == 'exit':
+            break
+        elif msg == 'menu':
+            sock.send(base_msg_encode(menu))
+        elif msg == 'lrs':
+            pass
+        elif msg == 'xg':
+            pass
+        elif msg == 'show':
+            global dict_key
+            sock.send(base_msg_encode(dict_key[passwd]))
+        msg_num = -1
+        global dict_msg
+        dict_msg[passwd].append((name ,msg))
+        for i in dict_msg[passwd]:
+            msg_num = msg_num + 1
+            if msg_num != len(dict_msg[passwd]):
+                if i[0] != name:
+                    sock.send(base_msg_encode(f'{i[0]}:{i[1]}'))
+
+
+
+def base_msg_encode(msg):
+    msg_utf = msg.encode('UTF-8')
+    msg_bas = base64.b64encode(msg_utf)
+    return msg_bas
+
+
+def base_msg_decode(msg_bas):
+    msg_utf = base64.b64decode(msg_bas)
+    msg = msg_utf.decode('UTF-8')
+    return msg
+
+
+if __name__ == "__main__":
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', 3389))
+    s.listen(10)
+    print('Waiting for connection...')
+    while True:
+        # 接受一个新连接:
+        sock, addr = s.accept()
+        # 创建新线程来处理TCP连接:
+        t = threading.Thread(target=tcplink, args=(sock, addr))
+        t.start()
+        for i in dict_key:
+            if dict_key[i] == []:
+                dict_key.pop(dict_key[i])
+                dict_msg.pop(dict_key[i])
